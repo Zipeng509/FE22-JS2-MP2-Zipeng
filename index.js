ef6bc5749201c6998ec0015a70aa0aa0ff3bb156
Zipeng509 <106557767+Zipeng509@users.noreply.github.com>
2023-05-11 18:57:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,191 +1,24 @@
-import { CatTamagotchi, DogTamagotchi } from "./modules/tamagotchi.js";
+import { Tamagotchi } from "./modules/tamagotchi.js";
 
-const inputNameField = document.getElementById("input-name");
-const selectTypeField = document.getElementById("select-type");
-const addBtn = document.getElementById("add-btn");
-const tamagotchiContainer = document.getElementById("tamagotchi-container");
-const formElement = document.getElementById("form");
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+  const nameInput = document.querySelector("input");
+  const typeSelect = document.querySelector("select");
+
+  const name = nameInput.value;
+  const type = typeSelect.value;
+
+  if (name === "") {
+    alert("Please fill the name field");
+    return;
+  }
+
+  const newTama = new Tamagotchi(name, type);
+  newTama.startPlaying();
+
+  nameInput.value = "";
+  typeSelect.value = "";
 });
 
-const tamagotchiArray = [];
-function hasTamagotchiName(name) {
-    return tamagotchiArray
-        .map(tamagotchi => tamagotchi.getName())
-        .includes(name);
-};
-
-addBtn.addEventListener("click", () => {
-
-    const name = inputNameField.value;
-    const type = selectTypeField.options[selectTypeField.selectedIndex].value;
-
-    if (name === "") {
-        alert("Please fill the name field")
-        return;
-    }
-
-    let tamagotchi;
-
-    const isCat = type === "cat";
-
-    if (isCat) {
-        tamagotchi = new CatTamagotchi(name)
-    };
-
-    const isDog = type === "dog";
-
-    if (isDog) {
-        tamagotchi = new DogTamagotchi(name)
-    };
-
-    if (!isCat && !isDog) {
-        return;
-    };
-
-    if (hasTamagotchiName(name)) {
-        alert(`Name is already taken "${name}", please use another name`)
-        return;
-    }
-    // console.log("asdasd")
-
-    tamagotchiArray.push(tamagotchi);
-    const tamagotchiUi = createTamagotchiUi(tamagotchi);
-    tamagotchiContainer.innerHTML += tamagotchiUi;
-    
-    const startBtn = getTamagotchiStartBtn(tamagotchi);
-    const hungerLevelElement = getTamagotchiHungerLevelElement(tamagotchi);
-    const hapinessElement = getTamagotchiHapinessElement(tamagotchi);
-
-    let hasStarted = false;
-    startBtn.addEventListener("click", () => {
-        hasStarted = true;
-        const id1 = setInterval(() => {
-            tamagotchi.decreaseHunger();
-            const hunger = tamagotchi.getHunger();
-            hungerLevelElement.innerText = hunger;
-
-            if (tamagotchi.hasDied()) {
-                clearId1();
-                clearId2();
-                const element = document.getElementById(tamagotchi.getName())
-                element.className += "died"
-                alert("You have lost");
-            }
-        }, 1000);
-
-        function clearId1() {
-            clearTimeout(id1)
-        };
-
-        const id2 = setInterval((a) => {
-            tamagotchi.decreaseHapiness()
-            const hapiness = tamagotchi.gethapiness();
-            hapinessElement.innerText = hapiness;
-            if (tamagotchi.hasDied()) {
-                clearId2()
-                clearId1()
-                alert("You have lost");
-            }
-        }, 2000);
-
-        function clearId2() {
-            clearTimeout(id2)
-        }
-    });
-
-    const playBtn = getTamagotchiPlayBtn(tamagotchi);
-    playBtn.addEventListener("click", () => {
-        if (hasStarted) {
-            tamagotchi.play();
-            const hapiness = tamagotchi.gethapiness();
-            hapinessElement.innerText = hapiness;
-        }
-    });
-
-    const feedBtn = getTamagotchiFeedBtn(tamagotchi);
-    feedBtn.addEventListener("click", () => {
-        if (hasStarted) {
-            tamagotchi.feed();
-            const hunger = tamagotchi.getHunger();
-            hungerLevelElement.innerText = hunger;
-        }
-    })
-});
-
-function getTamagotchiStartBtnId(tamagotchi) {
-    return `${tamagotchi.getName()}-start-btn`;
-};
-
-function getTamagotchiStartBtn(tamagotchi) {
-    return document.getElementById(getTamagotchiStartBtnId(tamagotchi))
-};
-
-function getTamagotchiFeedBtnId(tamagotchi) {
-    return `${tamagotchi.getName()}-feed-btn`
-};
-
-function getTamagotchiFeedBtn(tamagotchi) {
-    return document.getElementById(getTamagotchiFeedBtnId(tamagotchi))
-};
-
-function getTamagotchiPlayBtnId(tamagotchi) {
-    return `${tamagotchi.getName()}-play-btn`
-};
-
-function getTamagotchiPlayBtn(tamagotchi) {
-    return document.getElementById(getTamagotchiPlayBtnId(tamagotchi))
-};
-
-function getTamagotchiHungerLevelId(tamagotchi) {
-    return `${tamagotchi.getName()}-hunger-level`
-};
-
-function getTamagotchiHungerLevelElement(tamagotchi) {
-    return document.getElementById(getTamagotchiHungerLevelId(tamagotchi))
-};
-
-function getTamagotchiHapinessId(tamagotchi) {
-    return `${tamagotchi.getName()}-hapiness-level`
-};
-
-function getTamagotchiHapinessElement(tamagotchi) {
-    return document.getElementById(getTamagotchiHapinessId(tamagotchi))
-};
-
-function createTamagotchiUi(tamagotchi) {
-    const title = tamagotchi.getTitle()
-    const hungerLevel = tamagotchi.getHunger()
-    const hapinessLevel = tamagotchi.gethapiness()
-    const type = tamagotchi.getType();
-    const feedBtnId = getTamagotchiFeedBtnId(tamagotchi);
-    const startBtnId = getTamagotchiStartBtnId(tamagotchi)
-    const playBtnId = getTamagotchiPlayBtnId(tamagotchi) 
-    const id = tamagotchi.getName()
-    return `
-        <div id=${id} class="${type.toLowerCase()} tamagotchi">
-            <h4>
-                ${title}
-            </h4>
-            <p>
-                Hunger level: <span id="${getTamagotchiHungerLevelId(tamagotchi)}">${hungerLevel}</span>
-            </P>
-            <p>
-                Hapiness level: <span id="${getTamagotchiHapinessId(tamagotchi)}">${hapinessLevel}</span>
-            </P>
-            <div>
-                <button id="${feedBtnId}">
-                    Feed
-                </button>
-                <button id="${playBtnId}">
-                    Play
-                </button>
-                <button id="${startBtnId}">
-                    Start
-                </button>
-            <div>
-        </div>
-    `
-};
--- modules/tamagotchi.js
+++ modules/tamagotchi.js
@@ -2,69 +2,130 @@ class Tamagotchi {
     #type;
     #name;
     #hunger;
-    #hapiness;
+    #happiness;
+    #hungerId;
+    #happinessId;
+    #container;
+  
     constructor(type, name) {
-        this.#type = type
-        this.#name = name
-        this.#hunger = 10;
-        this.#hapiness = 10;
+      this.#type = type;
+      this.#name = name;
+      this.#hunger = 10;
+      this.#happiness = 10;
     }
-    getName() {
-        return this.#name
+  
+    //Kicka igång
+    startPlaying() {
+      const tamagotchiContainer = document.querySelector("#tamagotchi-container");
+      tamagotchiContainer.classList.remove("hidden");
+      tamagotchiContainer.classList.add("container");
+  
+      this.#container = document.createElement("div");
+      this.#container.classList.add(this.#type);
+  
+      const tamagotchiCard = document.createElement("div");
+      tamagotchiCard.classList.add("tamagotchiCard");
+      tamagotchiCard.style.backgroundColor = "turquoise";
+  
+      const h3 = document.createElement("h3");
+      const h4 = document.createElement("h4");
+      h4.innerText = `${this.#name}:${this.#type}`;
+      const hungerLevel = document.createElement("p");
+      hungerLevel.innerText = `Hunger level: ${this.#hunger}/10`;
+      const happinessLevel = document.createElement("p");
+      happinessLevel.innerText = `Happiness level: ${this.#happiness}/10`;
+  
+      tamagotchiCard.append(h3, h4, hungerLevel, happinessLevel);
+  
+      const feedBtn = this.createButton("Feed", "dot");
+      feedBtn.addEventListener("click", () => {
+        this.feed(hungerLevel);
+      });
+      feedBtn.style.marginRight = ("8px")
+  
+      const playBtn = this.createButton("Play", "dot");
+      playBtn.addEventListener("click", () => {
+        this.play(happinessLevel);
+      });
+  
+      this.#container.append(tamagotchiCard, feedBtn, playBtn);
+      tamagotchiContainer.append(this.#container);
+  
+      this.startInterval(hungerLevel, happinessLevel);
     }
-    getTitle() {
-        return `${this.#type}: ${this.#name}`;
+  
+    createButton(text, className) {
+      const button = document.createElement("button");
+      button.innerText = text;
+      button.classList.add(className);
+      return button;
     }
-    getHunger() {
-        return this.#hunger;
+  
+    startInterval(hungerLevel, happinessLevel) {
+      this.#hungerId = setInterval(() => {
+        this.updateLevel(hungerLevel, "hunger");
+      }, 2000);
+  
+      this.#happinessId = setInterval(() => {
+        this.updateLevel(happinessLevel, "happiness");
+      }, 3500);
     }
-    gethapiness() {
-        return this.#hapiness;
+  
+    updateLevel(level, type) {
+      if (type === "hunger") {
+        this.#hunger--;
+        level.innerText = `Hunger level: ${this.#hunger}/10`;
+        if (this.#hunger === 0) this.dead();
+      } else if (type === "happiness") {
+        this.#happiness--;
+        level.innerText = `Happiness level: ${this.#happiness}/10`;
+        if (this.#happiness === 0) this.dead();
+      }
     }
-    getType() {
-        return this.#type;
+  
+
+    //Mata tamagotchi för att hålla liv
+    feed(level) {
+      if (this.#hunger > 0 && this.#hunger < 10) {
+        this.#hunger++;
+        level.innerText = `Hunger level: ${this.#hunger}/10`;
+      } else {
+        level.innerText = `Hunger level: ${this.#hunger}/10`;
+      }
     }
-    decreaseHunger() {
-        if (!this.hasDied()) {
-            this.#hunger -= 1;
+  
+
+    //Leka med tamagotchi för att hålla liv
+    play(level) {
+      if (this.#happiness > 0 && this.#happiness < 10) {
+        this.#happiness++;
+        level.innerText = `Happiness level: ${this.#happiness}/10`;
+      } else {
+        level.innerText = `Happiness level: ${this.#happiness}/10`;
+      }
+    }
+  
+
+    //Disabled knapparna när den dö
+    dead() {
+        clearInterval(this.#hungerId);
+        clearInterval(this.#happinessId);
+      
+        this.#container.classList.add("dead");
+      
+        for (const childNode of this.#container.childNodes) {
+          if (childNode.tagName === "BUTTON") {
+            childNode.disabled = true;
+          }
+          if (childNode.classList.contains("screen")) {
+            const text = childNode.firstChild;
+            text.textContent = "R.I.P";
+            text.classList.add("rip");
+          }
         }
-    }
-    decreaseHapiness() {
-        if (!this.hasDied()) {
-            this.#hapiness -= 1;
-        }
-    }
-    hasDied() {
-        return this.#hapiness === 0 || this.#hunger === 0;
-    }
-    play() {
-        if (!this.hasDied() && this.#hapiness < 10) {
-            this.#hapiness += 1
-        }
-    }
-    feed() {
-        if (!this.hasDied() && this.#hunger < 10) {
-            this.#hunger += 1
-        }
-    }
-};
+      }
+            
 
-class CatTamagotchi extends Tamagotchi {
-    constructor(name) {
-        super("Cat", name)
-    }
-};
+}
 
-class DogTamagotchi extends Tamagotchi {
-    constructor(name) {
-        super("Dog", name)
-    }
-};
-
-
-
-
-export {
-    CatTamagotchi,
-    DogTamagotchi
-};
+export {Tamagotchi}
